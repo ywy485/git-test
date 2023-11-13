af17bb4a60adfce9a6b35c032c7b01d5cff5bc1f
ywy <ywy@qq.com>
2023-11-13 14:10:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 杨蔚原
+ * @version 1.0 on   2023-11-13 14:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
